8f5d07552f45197887e19634ba9882cc31538e94
sushi38742 <104280378+sushi38742@users.noreply.github.com>
2026-06-13 17:27:32 -0400

    Add webpack config for xlsx + fix next.config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-module.exports = nextConfig
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+      crypto: false,
+    };
+    return config;
+  },
+};
+module.exports = nextConfig;
